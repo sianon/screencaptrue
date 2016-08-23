8d28c85d8e82993daa213857fb1d44ece264a9a1
Xlivemaster <chiangkun@outlook.com>
2016-08-23 16:01:39 +0800

    合并 Signed-off-by: Xlivemaster <chiangkun@outlook.com>

--- screen_captrue/msg_head.h
+++ screen_captrue/msg_head.h
@@ -4,6 +4,10 @@
 
 enum AppMsg
 {
-	kAM_ShowTaskMsg = WM_APP + 10,		// 显示托盘图标
+	kAM_Init = WM_APP + 10,
+	kAM_ShowTaskMsg,		// 显示托盘图标
 	kAM_ExitForPop,			// 托盘右键菜单的退出消息
+	kAM_MainForPop,			// 托盘右键菜单 打开主界面
+	kAM_BeginForPop,		// 托盘右键菜单 开始
+	kAM_EndForPop,			// 托盘右键菜单 停止
 };
