dac5452f367e5882cb5ee7cf4dcfd96c3bf95f88
jhp1900 <jhp1900@hotmail.com>
2016-08-25 17:35:29 +0800

    添加：Crypt 工程及测试工程；
    Signed-off-by: jhp1900 <jhp1900@hotmail.com>

--- screen_captrue/ivga.cpp
+++ screen_captrue/ivga.cpp
@@ -2,8 +2,8 @@
 #include "manager.h"
 #include "ivga_engine.h"
 #include "hook.h"
-//#include "CryptKeyUtils.h"
-//#pragma comment(lib, "CryptKeyUtils.lib")
+#include "crypt_key_utils/CryptKeyUtils.h"
+#pragma comment(lib, "CryptKeyUtils.lib")
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -15,22 +15,22 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	
 	CPaintManagerUI::SetInstance(hInstance);
 	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath() + _T("skin"));	
-	CPaintManagerUI::SetResourceZip(_T("default.x"));
+	//CPaintManagerUI::SetResourceZip(_T("default.x"));
 
-	//DuiLib::CDuiString default_x_path = CPaintManagerUI::GetResourcePath() + L"default.x";
-	//if (PathFileExists(default_x_path)) {
-	//	FILE *file = nullptr;
-	//	if (!_tfopen_s(&file, default_x_path, _T("rb"))) {
-	//		BYTE buf[256 * 1024];
-	//		unsigned len = fread(buf, sizeof(BYTE), 256 * 1024, file);
-	//		len = Crypt("KINGHOO", buf, len, buf, 256 * 1024, FALSE);
-	//		fclose(file);
-	//		CPaintManagerUI::SetResourceZip(buf, len);
-	//		CPaintManagerUI::SetForceUseLocalResourceFile(true, false, true);
-	//	} else {
-	//		return false;
-	//	}
-	//}
+	DuiLib::CDuiString default_x_path = CPaintManagerUI::GetResourcePath() + L"default.x";
+	if (PathFileExists(default_x_path)) {
+		FILE *file = nullptr;
+		if (!_tfopen_s(&file, default_x_path, _T("rb"))) {
+			BYTE buf[256 * 1024];
+			unsigned len = fread(buf, sizeof(BYTE), 256 * 1024, file);
+			len = Crypt("KINGHOO", buf, len, buf, 256 * 1024, FALSE);
+			fclose(file);
+			CPaintManagerUI::SetResourceZip(buf, len);
+			CPaintManagerUI::SetForceUseLocalResourceFile(true, false, true);
+		} else {
+			return false;
+		}
+	}
 
 	Manager manager;
 	IVGAHook ivga_;
