ff84b2bc456f0b2677f06fa81cd86b1d39c58fa5
jhp1900 <jhp1900@hotmail.com>
2016-08-25 11:09:34 +0800

    修改：将界面文件打包成改为“.x”的 zip 压缩包；
    Signed-off-by: jhp1900 <jhp1900@hotmail.com>

--- screen_captrue/ivga.cpp
+++ screen_captrue/ivga.cpp
@@ -2,6 +2,8 @@
 #include "manager.h"
 #include "ivga_engine.h"
 #include "hook.h"
+//#include "CryptKeyUtils.h"
+//#pragma comment(lib, "CryptKeyUtils.lib")
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -13,6 +15,22 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	
 	CPaintManagerUI::SetInstance(hInstance);
 	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath() + _T("skin"));	
+	CPaintManagerUI::SetResourceZip(_T("default.x"));
+
+	//DuiLib::CDuiString default_x_path = CPaintManagerUI::GetResourcePath() + L"default.x";
+	//if (PathFileExists(default_x_path)) {
+	//	FILE *file = nullptr;
+	//	if (!_tfopen_s(&file, default_x_path, _T("rb"))) {
+	//		BYTE buf[256 * 1024];
+	//		unsigned len = fread(buf, sizeof(BYTE), 256 * 1024, file);
+	//		len = Crypt("KINGHOO", buf, len, buf, 256 * 1024, FALSE);
+	//		fclose(file);
+	//		CPaintManagerUI::SetResourceZip(buf, len);
+	//		CPaintManagerUI::SetForceUseLocalResourceFile(true, false, true);
+	//	} else {
+	//		return false;
+	//	}
+	//}
 
 	Manager manager;
 	IVGAHook ivga_;
@@ -25,6 +43,5 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	manager.ShowModal();
 	//manager.ShowWindow(SW_HIDE);
 	/**/
-
 	return 0;
 }
